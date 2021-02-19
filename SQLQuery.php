8506ee6ff6f49ac32b9c173def66b737d13f6d9d
Songül Keskin <33230597+songulkskn@users.noreply.github.com>
2021-02-19 23:13:56 +0300

    Update SQLQuery.php

--- SQLQuery.php
+++ SQLQuery.php
@@ -1,3 +1,39 @@
 <?php
+class SQLQuery{
+    private  $table="";
+    private  $bindings=[];
+    private  $offset;
+    private  $limit;
+    private $wheres =[];
+    private  $action="";
+
+    public function setTable(string $table){
+        return $this;
+    }
+    public function addBinding (string $key, string $value) {
+        return $this;
+    }
+    public function removeBinding (string $key) {
+        return $this;
+    }
+    public function setPaginate (int $limit, int $offset) {
+        return $this;
+    }
+    public function addWhere (string $column, string $operator, string $value) {
+        return $this;
+    }
+    public function delete(){
+
+    }
+    public function update(){
+
+    }
+    public function select(){
+        return $this;
+    }
+    public function compile(){
+
+    }
+}
 
 
