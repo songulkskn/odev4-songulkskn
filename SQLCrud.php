90ef9e7c20e64f0cef20bb81a3d8d3d4cf212210
Songül Keskin <33230597+songulkskn@users.noreply.github.com>
2021-02-19 23:12:30 +0300

    Update SQLCrud.php

--- SQLCrud.php
+++ SQLCrud.php
@@ -1,3 +1,18 @@
 <?php
+trait SQLCrud{
+    public function create(SQLQuery $query){
 
+    }
+    public function update(SQLQuery $query){
 
+    }
+    public function delete(SQLQuery $query){
+
+    }
+    public function get(SQLQuery $query){
+
+    }
+    public function first(SQLQuery $query){
+
+    }
+}
