4d17444b31c697fa01431ef07fc20b5155ced7bf
Songül Keskin <33230597+songulkskn@users.noreply.github.com>
2021-02-19 23:10:40 +0300

    Update MySQL.php

--- MySQL.php
+++ MySQL.php
@@ -1,3 +1,24 @@
 <?php
+class MySQL extends SQL{
+    use SQLCrud;
+    private $host;
+    private $post;
+    private $username;
+    private $password;
+    private $database;
+
+
+    public function __construct($host, $username, $password, $database){
+
+        $this -> host = $host;
+        $this -> username = $username;
+        $this -> password = $password;
+        $this -> database = $database;
+    }
+    public function __destruct(){
+
+    }
+}
+
 
 
