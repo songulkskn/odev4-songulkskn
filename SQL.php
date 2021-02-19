c4e8f08c4eb56955565dcd9a00867e26d1b2337d
Songül Keskin <33230597+songulkskn@users.noreply.github.com>
2021-02-19 23:12:05 +0300

    Update SQL.php

--- SQL.php
+++ SQL.php
@@ -1,3 +1,19 @@
 <?php
+abstract class SQL{
+    use PDOConnector;
+    public  $connectionStatus = false;
+
+    public abstract function create(SQLQuery $query);
+    public abstract function update(SQLQuery $query);
+    public abstract function delete(SQLQuery $query);
+
+    public function setParams(array $params){
+
+    }
+    public function isConnected(){
+
+    }
+
+}
 
 
