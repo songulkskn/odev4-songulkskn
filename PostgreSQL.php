f7be3e4f531fa04d5afefbf64214a9b7a7a34ed3
Songül Keskin <33230597+songulkskn@users.noreply.github.com>
2021-02-19 23:11:40 +0300

    Update PostgreSQL.php

--- PostgreSQL.php
+++ PostgreSQL.php
@@ -1,3 +1,22 @@
 <?php
+class PostgreSQL extends SQL  {
+    use SQLCrud;
+    private $host;
+    private $port;
+    private $username;
+    private $password;
+    private $database;
 
+    public function __construct($host, $username, $password, $database)
+    {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+    }
 
+    public function __destruct()
+    {
+
+    }
+}
